3d3aaf06abc3e8fd6b287b90cac15f90e93d207b
antonio <antonio@pykiss.com>
2016-08-01 01:14:06 +0200

    changed to ubuntu

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,10 @@
+ln -s `which nodejs` /usr/bin/node
 cd /meteor/programs/server
+echo "\ninstall"
 npm install
+echo "\nrebuild"
+npm rebuild
 chmod -R a+w /meteor
 cd /meteor
-node main.js
+echo "\nstart"
+nodejs main.js
